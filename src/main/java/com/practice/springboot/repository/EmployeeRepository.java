90cd5119407c38f79d22e0c313f98de4164d100d
Laith Hanna <laithhanna6@gmail.com>
2022-05-24 18:34:38 -0400

    added a custom method findByEmail to the EmployeeRepository interface

--- src/main/java/com/practice/springboot/repository/EmployeeRepository.java
+++ src/main/java/com/practice/springboot/repository/EmployeeRepository.java
@@ -3,10 +3,14 @@ package com.practice.springboot.repository;
 import com.practice.springboot.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /*There is no need to annotate this interface with @Repository.
 * Our interface extends JpaRepository and,
 * There is a class called "SimpleJpaRepository" which implements the JpaRepository interface.
 * That class is internally annotated with @Repository.*/
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Optional<Employee> findByEmail(String email);
 }
