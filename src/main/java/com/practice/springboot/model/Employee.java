473955a81f8d6928bc2a0f0f0a3f317248cfc6ab
Laith Hanna <laithhanna6@gmail.com>
2022-05-24 09:00:11 -0400

    Added JPA annotations to the Emoloyee class to make it a JPA entity

--- src/main/java/com/practice/springboot/model/Employee.java
+++ src/main/java/com/practice/springboot/model/Employee.java
@@ -2,14 +2,28 @@ package com.practice.springboot.model;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
 @Builder
+
+@Entity
+@Table(name = "employees")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false)
     private String lastName;
+
+    @Column(nullable = false)
     private String email;
 }
