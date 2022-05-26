4fb703fe37199cff7f90bebd27c82ecb1d5e4328
Laith Hanna <laithhanna6@gmail.com>
2022-05-26 09:46:46 -0400

    Added an exception to the SaveEmployee method

--- src/main/java/com/practice/springboot/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/practice/springboot/service/impl/EmployeeServiceImpl.java
@@ -1,10 +1,13 @@
 package com.practice.springboot.service.impl;
 
+import com.practice.springboot.exception.ResourceNotFoundException;
 import com.practice.springboot.model.Employee;
 import com.practice.springboot.repository.EmployeeRepository;
 import com.practice.springboot.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -14,6 +17,11 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee saveEmployee(Employee employee) {
+        //if the employee email is not unique then throw an exception
+        Optional<Employee> savedEmployee = employeeRepository.findByEmail(employee.getEmail());
+        if (savedEmployee.isPresent()) {
+            throw new ResourceNotFoundException("Employee already exists with the given email: " + employee.getEmail());
+        }
         return employeeRepository.save(employee);
     }
 }
