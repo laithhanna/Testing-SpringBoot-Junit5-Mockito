bbe1bf396f88cca89587da4c443a830117501e33
Laith Hanna <laithhanna6@gmail.com>
2022-05-28 14:55:45 -0400

    Controller layer testing: Created the createEmployee Rest api

---
+++ src/main/java/com/practice/springboot/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.practice.springboot.controller;
+
+import com.practice.springboot.model.Employee;
+import com.practice.springboot.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    //constructor based dependency injection
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+}
