4d6af5d62afe18d688a36f902c2b5f7ec793aad9
Laith Hanna <laithhanna6@gmail.com>
2022-05-29 21:01:55 -0400

    Controller layer testing: Refactoring JUnit tests by using static imports

--- src/test/java/com/practice/springboot/controller/EmployeeControllerTests.java
+++ src/test/java/com/practice/springboot/controller/EmployeeControllerTests.java
@@ -3,25 +3,24 @@ package com.practice.springboot.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.practice.springboot.model.Employee;
 import com.practice.springboot.service.EmployeeService;
-import org.hamcrest.CoreMatchers;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.BDDMockito;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @WebMvcTest
@@ -62,7 +61,7 @@ public class EmployeeControllerTests {
         //employee object is created in the setup method
 
         //stub the employeeService.saveEmployee() method
-        BDDMockito.given(employeeService.saveEmployee(ArgumentMatchers.any(Employee.class)))
+        given(employeeService.saveEmployee(any(Employee.class)))
                 .willAnswer((invocation) -> invocation.getArgument(0)); //will return the same object we pass
 
 
@@ -75,11 +74,11 @@ public class EmployeeControllerTests {
         //then - validate the response of the rest api
         //validate that the response contains valid status code. (In this case CREATED aka 201).
         //Then, validate that the response contain valid JSON values.
-        response.andDo(MockMvcResultHandlers.print()) //to print the rest api call and response on the terminal
-                .andExpect(MockMvcResultMatchers.status().isCreated())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName", CoreMatchers.is(employee.getFirstName())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName", CoreMatchers.is(employee.getLastName())))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.email", CoreMatchers.is(employee.getEmail())));
+        response.andDo(print()) //to print the rest api call and response on the terminal
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName", is(employee.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(employee.getLastName())))
+                .andExpect(jsonPath("$.email", is(employee.getEmail())));
         //note that the '$' symbol represent the root object aka the whole JSON object
     }
 
@@ -94,15 +93,15 @@ public class EmployeeControllerTests {
                 .email("will@gmail.com")
                 .build();
 
-        BDDMockito.given(employeeService.getAllEmployees()).willReturn(List.of(employee, employee2));
+        given(employeeService.getAllEmployees()).willReturn(List.of(employee, employee2));
 
         //when - action or the behavior we are testing
         ResultActions response = mockMvc.perform(get("/api/employees"));
 
         //then - verify the output
-        response.andDo(MockMvcResultHandlers.print())
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.size()", CoreMatchers.is(2)));
+        response.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()", is(2)));
                 //since the return is an array of JSON. $ represent an array of JSON so $.size give you size of that array
 
     }
