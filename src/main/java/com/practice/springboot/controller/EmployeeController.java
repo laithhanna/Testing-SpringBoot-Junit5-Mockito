300fbf03282342f1c298a7782579c0c8f80c7234
Laith Hanna <laithhanna6@gmail.com>
2022-05-29 23:21:46 -0400

    Controller layer testing: Created getEmployeeById REST api

--- src/main/java/com/practice/springboot/controller/EmployeeController.java
+++ src/main/java/com/practice/springboot/controller/EmployeeController.java
@@ -3,9 +3,11 @@ package com.practice.springboot.controller;
 import com.practice.springboot.model.Employee;
 import com.practice.springboot.service.EmployeeService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -27,5 +29,14 @@ public class EmployeeController {
     @GetMapping
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") long employeeId) {
+        //ResponseEntity represents the whole HTTP response: status code, headers, and body.
+        // As a result, we can use it to fully configure the HTTP response.
+        return employeeService.getEmployeeById(employeeId)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
